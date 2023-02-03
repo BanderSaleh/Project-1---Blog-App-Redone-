e8d70a310fdae693eb7f7a8ea5e833ecff672af4
Bander Saleh <bandersaleh93@gmail.com>
2023-02-02 21:37:17 -0800

    Step 1/4 Finalized Code

---
+++ Blog_App2/BlogPost.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blog_App2
+{
+    public class BlogPost
+    {
+        // Fields
+
+        string _header; // Holds Header value
+        string _body; // Holds Body value
+        DateTime _posted; // Holds time post was created
+
+        // Highlighted x2 Fields above^ and right clicked and created constructor below:
+        // The DateTime will get assigned within the constructor
+
+        // Constructor
+        public BlogPost(string header, string body)
+        {
+            _header = header;
+            _body = body;
+            _posted = DateTime.Now; // Here we assign the new value for DateTime
+        }
+
+        // Properties
+        public string Header { get => _header; set => _header = value; }
+        public string Body { get => _body; set => _body = value; }
+        public DateTime Posted { get => _posted; set => _posted = value; }
+
+        // Methods
+        // here we create a method that returns a string.
+        public string Post()
+        {
+            // Date - Header
+            // Body
+
+            string date = _posted.ToShortDateString();
+            string header = $"{date} - {_header}";
+            string space = $"\n\n";
+            string body = _body;
+
+            string full = header + space + body;
+
+            return full;
+        }
+
+
+
+    }
+}
---
+++ Blog_App2/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Blog_App2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreatePost_Click(object sender, RoutedEventArgs e)
+        {
+            // get the value from our Header TextBox
+            // get the value from our Body Run
+            string header = txtHeader.Text;
+            string body = runBody.Text;
+
+            // create a new BlogPost object
+            BlogPost bp = new BlogPost(header, body);
+
+            // display in our runDisplay by:
+            // Calling our post method to return a formatted post string
+            // Assign it to our runDisplay
+            runDisplay.Text = bp.Post();
+
+        }
+    }
+}
